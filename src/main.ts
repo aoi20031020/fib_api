d233f54611ba83df6f390e8153cae1c8876c1e7c
aoi20031020 <aoi20031020@gmail.com>
2024-10-12 22:04:49 +0900

    vercelサーバーレス設定

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { Handler } from 'aws-lambda';
+import * as serverless from 'aws-serverless-express';
+
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+  await app.init();
 }
-bootstrap();
+
+let cachedServer: Handler;
+
+async function createLambdaServer() {
+  await bootstrap();
+  return serverless.createServer(expressApp);
+}
+
+export const handler: Handler = async (event, context) => {
+  if (!cachedServer) {
+    cachedServer = await createLambdaServer();
+  }
+  return serverless.proxy(cachedServer, event, context);
+};
