7acee2da48f23ddaa2d1eeab2840d76118af0220
aoi20031020 <aoi20031020@gmail.com>
2024-10-12 22:10:13 +0900

    設定修正

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import * as serverlessExpress from 'aws-serverless-express';
 import { Handler } from 'aws-lambda';
-import * as serverless from 'aws-serverless-express';
 
 const expressApp = express();
 
@@ -19,12 +19,12 @@ let cachedServer: Handler;
 
 async function createLambdaServer() {
   await bootstrap();
-  return serverless.createServer(expressApp);
+  return serverlessExpress.createServer(expressApp);
 }
 
 export const handler: Handler = async (event, context) => {
   if (!cachedServer) {
     cachedServer = await createLambdaServer();
   }
-  return serverless.proxy(cachedServer, event, context);
+  return serverlessExpress.proxy(cachedServer, event, context);
 };
